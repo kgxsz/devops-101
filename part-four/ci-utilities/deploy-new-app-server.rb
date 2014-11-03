237ebd084f8e2ca2f4a4c9e3ee44a6c65fdf1729
Keigo Suzukawa <k.suzukawa@gmail.com>
2014-11-03 20:23:49 +0000

    Fix reference to command string

--- part-four/ci-utilities/deploy-new-app-server.rb
+++ part-four/ci-utilities/deploy-new-app-server.rb
@@ -8,7 +8,7 @@ def extract_subnet_id
                               --filters Name=tag:aws:cloudformation:logical-id,Values=Subnet \
                               --region eu-west-1 \
                               --output json"
-  subnets = JSON.parse(`describe_subnets_command`)["Subnets"]
+  subnets = JSON.parse(`#{describe_subnets_command}`)["Subnets"]
   subnet_id = subnets.first["SubnetId"]
   puts "Extracted subnet id: #{subnet_id}"
   return subnet_id
@@ -19,7 +19,7 @@ def extract_security_group_id
                                       --filters Name=tag:aws:cloudformation:logical-id,Values=SecurityGroup \
                                       --region eu-west-1 \
                                       --output json"
-  security_groups = JSON.parse(`describe_security_groups_command`)["SecurityGroups"]
+  security_groups = JSON.parse(`#{describe_security_groups_command}`)["SecurityGroups"]
   security_group_id = security_groups.first["GroupId"]
   puts "Extracted security group id: #{security_group_id}"
   return security_group_id
